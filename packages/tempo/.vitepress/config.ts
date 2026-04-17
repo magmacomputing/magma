e831d55f270cf497521ea15f630814491ee7c7fa
Michael McRae <michael@magmacomputing.com.au>
2026-04-17 20:00:55 +1000

    gh pages

--- packages/tempo/.vitepress/config.ts
+++ packages/tempo/.vitepress/config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
+  base: '/magma/',
   title: "Tempo",
   description: "The Professional Date-Time Library for Temporal",
   themeConfig: {
