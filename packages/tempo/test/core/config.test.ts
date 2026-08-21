e2a8fb778427bb10ab82faf5ba0b82daa979a1de
Michael McRae <michael@magmacomputing.com.au>
2026-08-21 14:29:47 +1000

    standard tests

--- packages/tempo/test/core/config.test.ts
+++ packages/tempo/test/core/config.test.ts
@@ -34,7 +34,7 @@ describe('#setConfig refactor verification', () => {
 
   test('should parse custom date layout registered via registry.layouts', () => {
     using _ = Tempo;
-    Tempo.init({ registry: { layouts: { dot_date: '{dd}.{mm}.{yy}' } } });
+    Tempo.init({ timeZone: 'UTC', locale: 'en-GB', registry: { layouts: { dot_date: '{dd}.{mm}.{yy}' } } });
     const date = new Tempo('04.08.2026');
     expect(date.isValid).toBe(true);
     expect(date.dd).toBe(4);
