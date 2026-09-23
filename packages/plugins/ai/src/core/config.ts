287fde64d27974be3a4d0ae413e2ffea2119c78b
Michael McRae <michael@magmacomputing.com.au>
2026-09-23 13:26:08 +1000

    upgrade tempo model to "120b"

--- packages/plugins/ai/src/core/config.ts
+++ packages/plugins/ai/src/core/config.ts
@@ -70,7 +70,7 @@ export const DEFAULT_PROVIDERS: Readonly<Record<string, Partial<AiProvider>>> = 
 	tempo: {
 		url: 'https://tempo.magmacomputing.com.au/api/ai/tempo',
 		models: {
-			default: 'openai/gpt-oss-20b',
+			default: 'openai/gpt-oss-120b',
 			fast: 'openai/gpt-oss-20b',
 			large: 'openai/gpt-oss-120b'
 		},
